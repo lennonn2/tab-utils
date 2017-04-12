5ff0fe4ac3b1c821f79e5d4be022357b743380fa
Niall Lennon <niall.lennon2@gmail.com>
2017-04-11 20:43:47 -0700

    Fix for multiple windows

--- bg.js
+++ bg.js
@@ -1,9 +1,9 @@
-var tabStack = [];
+var tabStack = {};
 
 chrome.commands.onCommand.addListener(function(command) {
   switch (command) {
     case 'toggle-tabs':
-      chrome.tabs.update(tabStack[1], {active: true});
+      toggleTabs();
       break;
     case 'pop-tab-out':
       popTabOut();
@@ -15,18 +15,33 @@ chrome.commands.onCommand.addListener(function(command) {
 });
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-  this.removeIdFromArray(tabStack, activeInfo.tabId);
-  tabStack.unshift(activeInfo.tabId);
+  chrome.windows.getCurrent(function(window) {
+    if (! tabStack[window.id]) {
+      tabStack[window.id] = [];
+    }
+    var arr = removeIdFromArray(tabStack[window.id], activeInfo.tabId);
+    arr.unshift(activeInfo.tabId);
+    tabStack[window.id] = arr;
+  });
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId) {
-  for (var i = 0; i <= tabStack.length; i++) {
-    if (tabStack[i] === tabId) {
-      tabStack.splice(i, 1);
-      return;
-    }
-  }
+  chrome.windows.getCurrent(function(window) {
+    removeIdFromArray(tabStack[window.id], tabId);
+  });
 });
+
+chrome.windows.onRemoved.addListener(function(window) {
+  delete tabStack[window];
+});
+
+function toggleTabs() {
+  chrome.windows.getCurrent(function(window) {
+    if (tabStack[window.id]) {
+      chrome.tabs.update(tabStack[window.id][1], {active: true});
+    }
+  });
+};
 
 function removeIdFromArray(arr, id) {
   arr.forEach(function(tabId, index) {
@@ -35,23 +50,23 @@ function removeIdFromArray(arr, id) {
       return;
     }
   });
+  return arr;
 };
 
 function moveTab(dir) {
   chrome.tabs.getSelected(function(tab) {
-    console.log(tab);
     chrome.tabs.move(tab.id, {index: tab.index+dir});
   });
-}
+};
 
 function popTabOut() {
   chrome.tabs.getSelected(function(tab) {
     chrome.windows.create({tabId: tab.id, state: 'maximized'});
   });
-}
+};
 
 function duplicateTab() {
   chrome.tabs.getSelected(function(tab) {
     chrome.tabs.duplicate(tab.id);
   });
-}
+};
